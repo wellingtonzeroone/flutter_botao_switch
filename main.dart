9d970a317e9ba57c40e2c475f3fc10df66f7963b
wellingtonzeroone <naovalido3@gmail.com>
2024-04-03 11:56:52 -0300

    enviando03.04

---
+++ main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: const SwitchExample(),
+    ),
+  );
+}
+
+class SwitchExample extends StatefulWidget {
+  const SwitchExample({super.key});
+
+  @override
+  State<SwitchExample> createState() => _SwitchExampleState();
+}
+
+class _SwitchExampleState extends State<SwitchExample> {
+  bool light0 = true;
+  bool light1 = true;
+
+  final MaterialStateProperty<Icon?> thumbIcon = MaterialStateProperty.resolveWith<Icon?>
+  (
+    (Set<MaterialState> states) {
+      if (states.contains(MaterialState.selected)) {
+        return const Icon(Icons.check);
+      }
+      return const Icon(Icons.close);
+    },
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Switch(
+            value: light0,
+            onChanged: (bool value) {
+              setState(() {
+                light0 = value;
+              });
+            },
+          ),
+          Switch(
+            thumbIcon: thumbIcon,
+            value: light1,
+            onChanged: (bool value) {
+              setState(() {
+                light1 = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
